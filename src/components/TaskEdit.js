f5f64afe80aaec3dd6f8cb9b755c7eda34059fbb
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-02 15:34:26 +0300

    Update TaskEdit.js

--- src/components/TaskEdit.js
+++ src/components/TaskEdit.js
@@ -4,8 +4,8 @@ function TaskEdit({ task, onSaveTask }) {
   const [desc, setDesc] = useState("");
   const [date, setDate] = useState("");
 
-  const saveTask = (e) => {
-    e.preventDefault();
+  const saveTask = (event) => {
+    event.preventDefault();
     onSaveTask({ desc: desc, date: date });
 
     setDesc("");
